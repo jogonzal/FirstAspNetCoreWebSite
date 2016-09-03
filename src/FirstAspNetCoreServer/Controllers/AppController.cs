bb237f61568e5f6360475a25725ffe07b2322363
Jorge Aguirre <jogonzal@microsoft.com>
2016-09-02 17:36:43 -0700

    Initial commit - controllers working

---
+++ src/FirstAspNetCoreServer/Controllers/AppController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAspNetCoreServer.Controllers
+{
+	public class AppController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			return View();
+		}
+	}
+}
